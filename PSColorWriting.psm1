4da289764642dac2e739e9d83f2767e5fd0ca6ce
manathan-software-GmbH <developer@manathan.net>
2014-02-28 17:41:15 +0100

    added read-prompt-color

--- PSColorWriting.psm1
+++ PSColorWriting.psm1
@@ -1,3 +1,4 @@
+
 function Write-Output-Color
 {
     # DO NOT SPECIFY param(...)
@@ -42,4 +43,51 @@ function Write-Output-Color
         Write-Host $sofar -foreground $color -nonewline
     }
 }
+
+function Read-Prompt-Color
+{
+    # DO NOT SPECIFY param(...)
+    #    we parse colors ourselves.
+
+    $allColors = ("-Black",   "-DarkBlue","-DarkGreen","-DarkCyan","-DarkRed","-DarkMagenta","-DarkYellow","-Gray",
+                  "-Darkgray","-Blue",    "-Green",    "-Cyan",    "-Red",    "-Magenta",    "-Yellow",    "-White")
+    $foreground = (Get-Host).UI.RawUI.ForegroundColor # current foreground
+    $color = $foreground
+    [bool]$nonewline = $false
+    $sofar = ""
+    $total = ""
+
+    foreach($arg in $args)
+    {
+        if ($arg -eq "-nonewline") { $nonewline = $true }
+        elseif ($arg -eq "-foreground")
+        {
+            if ($sofar) { Write-Host $sofar -foreground $color -nonewline }
+            $color = $foregrnd
+            $sofar = ""
+        }
+        elseif ($allColors -contains $arg)
+        {
+            if ($sofar) { Write-Host $sofar -foreground $color -nonewline }
+            $color = $arg.substring(1)
+            $sofar = ""
+        }
+        else
+        {
+            $sofar += "$arg "
+            $total += "$arg "
+        }
+    }
+    # last bit done special
+    if (!$nonewline)
+    {
+       Read-Host -prompt $sofar
+    }
+    elseif($sofar)
+    {
+        Write-Host $sofar -foreground $color -nonewline
+    }
+}
+
 Export-ModuleMember -function Write-Output-Color
+Export-ModuleMember -function Read-Prompt-Color
